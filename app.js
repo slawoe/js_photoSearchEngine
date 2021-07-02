4c13ef559cd55988d313b7b1640664817d590589
slawoe <info@slawomirernst.de>
2021-07-02 09:44:55 +0200

    Add search fetch

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const auth = "INSERT YOUR API_KEY";
+const auth = "ENTER YOUR API_KEY";
+let searchValue = "";
 const gallery = document.querySelector(".gallery");
 const mode = document.querySelector("#mode");
 const searchInput = document.querySelector(".search-input");
 const submitButton = document.querySelector(".submit-button");
+const form = document.querySelector(".search-form");
 const toggleSwitch = document.querySelector(
   '.theme-toggle input[type="checkbox"]'
 );
 const currentTheme = localStorage.getItem("theme")
   ? localStorage.getItem("theme")
   : null;
+
+// EVENT LISTENER
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+// UPDATE INPUT
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 // THEME
 if (currentTheme) {
@@ -38,6 +53,26 @@ toggleSwitch.addEventListener("change", switchTheme, false);
 async function getPhotos() {
   const fetchPhotos = await fetch(
     "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await fetchPhotos.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.medium}></img><p class="photographer">Photographer: ${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const fetchPhotos = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
     {
       method: "GET",
       headers: {
